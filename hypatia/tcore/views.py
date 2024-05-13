fe23c4c9f68af677db664aa599b2dbecac136476
betulsener <betulsener1@gmail.com>
2024-05-13 23:16:49 +0300

    update
    form data saved to database

--- hypatia/tcore/views.py
+++ hypatia/tcore/views.py
@@ -1,7 +1,10 @@
 from typing import Any
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.views.generic import ListView, TemplateView
-from tcore.models import Slider, About
+from tcore.models import Slider, About, Input
+from django.contrib import messages
 
 class IndexView(ListView):
     template_name= "index.html"
@@ -30,4 +33,27 @@ class BlogView(TemplateView):
 
 class ContactView(TemplateView):
     template_name= "input.html"
+    def post(self, request, *args, **kwargs):
+        full_name = request.POST.get('fullName')
+        job = request.POST.get('job')
+        email = request.POST.get('email')
+        title = request.POST.get('title')
+        message = request.POST.get('message')
+
+        try:
+            input_instance = Input.objects.create(
+                full_name=full_name,
+                job=job,
+                email=email,
+                title=title,
+                message=message
+            )
+            messages.success(request,'İletiniz başarıyla gönderildi.')
+
+        except Exception as e:
+            messages.error(request, f'Mesaj gönderimi başarısız oldu: {e}')
+
+        return HttpResponseRedirect(reverse('input'))
+
+
 
